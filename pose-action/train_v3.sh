00d671033d1a1b5bb9073c408ea6b8ade2c7c19c
Shirley-LiyueShen <liyues@stanford.edu>
2017-02-14 14:17:49 -0800

    bash file

--- pose-action/train_v3.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-/vision/u/liyues/software/caffe/build/tools/caffe train --solver=train_v3_solver.prototxt --gpu=$1 --weights=../model/_trained_COCO/pose_iter_440000.caffemodel 2>&1 | tee ./output_train_v3.txt
